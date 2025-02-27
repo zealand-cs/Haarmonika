476b40758c91d7e0d810d9d0d64bd844b237c50b
William Jelgren <william@jelgren.dk>
2025-02-27 11:46:10 +0100

    Created Employee Service

---
+++ src/main/java/dk/haarmonika/haarmonika/backend/services/EmployeeService.java
@@ -0,0 +1,23 @@
+package dk.haarmonika.haarmonika.backend.services;
+
+import dk.haarmonika.haarmonika.backend.db.daos.EmployeeDao;
+import dk.haarmonika.haarmonika.backend.db.entities.Employee;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class EmployeeService {
+    private final EmployeeDao employeeDao;
+
+    public EmployeeService(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    public List<Employee> getAllEmployess() throws SQLException {
+        return employeeDao.getAll(null);
+    }
+
+    public void createEmployee(Employee employee) throws SQLException {
+        employeeDao.save(employee);
+    }
+}
