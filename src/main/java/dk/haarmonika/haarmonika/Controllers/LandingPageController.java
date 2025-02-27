bd9a154e7930fe09dd986648fc132fa2f4c0f70b
McImp <Magnus_petersen@outlook.com>
2025-02-27 09:24:51 +0100

    SwitchScene method implemented and fixed how program starts and implements scenes, fxml files and controllers

--- src/main/java/dk/haarmonika/haarmonika/Controllers/LandingPageController.java
+++ src/main/java/dk/haarmonika/haarmonika/Controllers/LandingPageController.java
@@ -3,12 +3,17 @@ package dk.haarmonika.haarmonika.Controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LandingPageController implements ControllerInterface {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void refresh() {
+
     }
 }
