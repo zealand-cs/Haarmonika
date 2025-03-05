e041b770c22803edd638aed4149dbdcad099e83a
William Jelgren <william@jelgren.dk>
2025-03-05 18:46:46 +0100

    Merged divergent branches

--- src/main/java/dk/haarmonika/haarmonika/backend/services/BookingService.java
+++ src/main/java/dk/haarmonika/haarmonika/backend/services/BookingService.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 
 @Service
 public class BookingService implements IBookingService{
-
     private final IBookingDao bookingDao;
     private final BookingValidator bookingValidator;
 
